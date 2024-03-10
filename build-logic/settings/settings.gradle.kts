6c3c249fe94eda015af1eef810b66b254664cb56
Alexey Illarionov <alexey@0xdc.ru>
2024-03-10 17:07:34 +0300

    Add workaround for https://github.com/gradle/gradle/issues/26020
    Kotlin tasks stay up-to-date when libs.versions.toml file is modified

--- build-logic/settings/settings.gradle.kts
+++ build-logic/settings/settings.gradle.kts
@@ -4,6 +4,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+// Workaround for https://github.com/gradle/gradle/issues/26020
+buildscript {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$embeddedKotlinVersion")
+    }
+}
+
 dependencyResolutionManagement {
     repositories.gradlePluginPortal()
     versionCatalogs {
