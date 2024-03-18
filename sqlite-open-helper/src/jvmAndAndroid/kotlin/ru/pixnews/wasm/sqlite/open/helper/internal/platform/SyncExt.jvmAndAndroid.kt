0ff341ed3443a42b6e36155dddff66b793c247a0
Alexey Illarionov <alexey@0xdc.ru>
2024-03-18 22:11:07 +0300

    Move SQLiteClosable to common

---
+++ sqlite-open-helper/src/jvmAndAndroid/kotlin/ru/pixnews/wasm/sqlite/open/helper/internal/platform/SyncExt.jvmAndAndroid.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2024, the wasm-sqlite-open-helper project authors and contributors. Please see the AUTHORS file
+ * for details. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package ru.pixnews.wasm.sqlite.open.helper.internal.platform
+
+internal actual inline fun <R> synchronized(lock: Any, block: () -> R): R = kotlin.synchronized(lock, block)
