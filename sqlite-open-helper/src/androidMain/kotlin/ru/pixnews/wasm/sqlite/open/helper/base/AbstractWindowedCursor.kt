0e5d0d2e28033d3c99b1663e1b987a2991d7a6e3
Alexey Illarionov <alexey@0xdc.ru>
2024-03-19 12:11:30 +0300

    Merge AbsrtactCursor and AbstractWindowedCursor

--- sqlite-open-helper/src/androidMain/kotlin/ru/pixnews/wasm/sqlite/open/helper/base/AbstractWindowedCursor.kt
+++ sqlite-open-helper/src/androidMain/kotlin/ru/pixnews/wasm/sqlite/open/helper/base/AbstractWindowedCursor.kt
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+@file:Suppress("FUNCTION_BOOLEAN_PREFIX")
+
 package ru.pixnews.wasm.sqlite.open.helper.base
 
 /*
@@ -13,10 +15,18 @@ package ru.pixnews.wasm.sqlite.open.helper.base
  * Licensed under the Apache License, Version 2.0 (the "License")
  */
 
+import android.content.ContentResolver
 import android.database.CharArrayBuffer
+import android.database.ContentObserver
+import android.database.Cursor
+import android.database.CursorIndexOutOfBoundsException
+import android.database.DataSetObserver
 import android.database.StaleDataException
+import android.net.Uri
+import android.os.Bundle
 import ru.pixnews.wasm.sqlite.open.helper.internal.cursor.CursorWindow
 import ru.pixnews.wasm.sqlite.open.helper.internal.cursor.NativeCursorWindow
+import java.lang.ref.WeakReference
 
 /**
  * A base class for Cursors that store their data in [android.database.CursorWindow]s.
@@ -37,7 +47,19 @@ import ru.pixnews.wasm.sqlite.open.helper.internal.cursor.NativeCursorWindow
  */
 internal abstract class AbstractWindowedCursor(
     private val windowFactory: (name: String?) -> CursorWindow,
-) : AbstractCursor() {
+) : Cursor {
+    protected var pos: Int = -1
+    protected var closed: Boolean = false
+
+    @Deprecated("deprecated in AOSP but still used for non-deprecated methods")
+    private var contentResolver: ContentResolver? = null
+    private var notifyUri: Uri? = null
+    private val selfObserverLock = Any()
+    private var selfObserver: ContentObserver? = null
+    private var selfObserverRegistered = false
+    private val dataSetObservable = DataSetObservable()
+    private val contentObservable = ContentObservable()
+
     /**
      * The cursor window owned by this cursor.
      */
@@ -65,6 +87,12 @@ internal abstract class AbstractWindowedCursor(
             }
         }
 
+    abstract override fun getCount(): Int
+
+    abstract override fun getColumnNames(): Array<String>
+
+    override fun getColumnCount(): Int = columnNames.size
+
     override fun getBlob(column: Int): ByteArray {
         checkPosition()
         return requireWindow().getBlob(pos, column) ?: byteArrayOf()
@@ -82,8 +110,6 @@ internal abstract class AbstractWindowedCursor(
      *
      * The buffer is populated as follows:
      *
-     *  * If the buffer is too small for the value to be copied, then it is
-     * automatically resized.
      *  * If the field is of type [Cursor.FIELD_TYPE_NULL], then the buffer
      * is set to an empty string.
      *  * If the field is of type [Cursor.FIELD_TYPE_STRING], then the buffer
@@ -96,8 +122,7 @@ internal abstract class AbstractWindowedCursor(
      * set to a string representation of the floating-point value in decimal, obtained by
      * formatting the value with the `printf` family of functions using
      * format specifier `%g`.
-     *  * If the field is of type [Cursor.FIELD_TYPE_BLOB], then a
-     * [SQLiteException] is thrown.
+     *  * If the field is of type [Cursor.FIELD_TYPE_BLOB], then [SQLiteException] is thrown.
      *
      */
     override fun copyStringToBuffer(columnIndex: Int, buffer: CharArrayBuffer) {
@@ -139,8 +164,19 @@ internal abstract class AbstractWindowedCursor(
         return requireWindow().getType(pos, column).id
     }
 
-    override fun checkPosition() {
-        super.checkPosition()
+    override fun getPosition(): Int = pos
+
+    /**
+     * This function throws CursorIndexOutOfBoundsException if the cursor position is out of bounds.
+     * Subclass implementations of the get functions should call this before attempting to
+     * retrieve data.
+     *
+     * @throws CursorIndexOutOfBoundsException
+     */
+    protected open fun checkPosition() {
+        if (-1 == pos || count == pos) {
+            throw CursorIndexOutOfBoundsException(pos, count)
+        }
         requireWindow()
     }
 
@@ -153,14 +189,211 @@ internal abstract class AbstractWindowedCursor(
      * Creates a new window.
      *
      * @param name The window name.
-     * @hide
      */
     protected fun createWindow(name: String?) {
         window = windowFactory(name)
     }
 
-    override fun onDeactivateOrClose() {
-        super.onDeactivateOrClose()
+    /**
+     * This function is called every time the cursor is successfully scrolled
+     * to a new position, giving the subclass a chance to update any state it
+     * may have.  If it returns false the move function will also do so and the
+     * cursor will scroll to the beforeFirst position.
+     *
+     *
+     * This function should be called by methods such as [.moveToPosition],
+     * so it will typically not be called from outside of the cursor class itself.
+     *
+     *
+     * @param oldPosition The position that we're moving from.
+     * @param newPosition The position that we're moving to.
+     * @return True if the move is successful, false otherwise.
+     */
+    abstract fun onMove(oldPosition: Int, newPosition: Int): Boolean
+
+    override fun move(offset: Int): Boolean = moveToPosition(pos + offset)
+
+    override fun moveToFirst(): Boolean = moveToPosition(0)
+
+    override fun moveToLast(): Boolean = moveToPosition(count - 1)
+
+    override fun moveToNext(): Boolean = moveToPosition(pos + 1)
+
+    override fun moveToPrevious(): Boolean = moveToPosition(pos - 1)
+
+    override fun isFirst(): Boolean = pos == 0 && count != 0
+
+    override fun isLast(): Boolean = count.let { cnt ->
+        pos == (cnt - 1) && cnt != 0
+    }
+
+    override fun isBeforeFirst(): Boolean = count == 0 || pos == -1
+
+    override fun isAfterLast(): Boolean = count == 0 || pos == count
+
+    @Suppress("ReturnCount")
+    override fun moveToPosition(position: Int): Boolean {
+        // Make sure position isn't past the end of the cursor
+        val count = count
+        if (position >= count) {
+            pos = count
+            return false
+        }
+
+        // Make sure position isn't before the beginning of the cursor
+        if (position < 0) {
+            pos = -1
+            return false
+        }
+
+        // Check for no-op moves, and skip the rest of the work for them
+        if (position == pos) {
+            return true
+        }
+
+        val positionReachable = onMove(pos, position)
+        pos = if (!positionReachable) {
+            -1
+        } else {
+            position
+        }
+
+        return positionReachable
+    }
+
+    override fun getColumnIndexOrThrow(columnName: String): Int {
+        val index = getColumnIndex(columnName)
+        require(index >= 0) { "column '$columnName' does not exist" }
+        return index
+    }
+
+    override fun getColumnIndex(columnName: String): Int {
+        // Hack according to some mysterious internal bug903852
+        val tableColumnName = columnName.substringAfterLast(".")
+        return columnNames.indexOfFirst { it.equals(tableColumnName, ignoreCase = true) }
+    }
+
+    override fun getColumnName(columnIndex: Int): String = columnNames[columnIndex]
+
+    override fun registerContentObserver(observer: ContentObserver) {
+        contentObservable.registerObserver(observer)
+    }
+
+    override fun unregisterContentObserver(observer: ContentObserver) {
+        // cursor will unregister all observers when it close
+        if (!closed) {
+            contentObservable.unregisterObserver(observer)
+        }
+    }
+
+    override fun registerDataSetObserver(observer: DataSetObserver) {
+        dataSetObservable.registerObserver(observer)
+    }
+
+    override fun unregisterDataSetObserver(observer: DataSetObserver) {
+        dataSetObservable.unregisterObserver(observer)
+    }
+
+    /**
+     * Subclasses must call this method when they finish committing updates to notify all
+     * observers.
+     *
+     * @param selfChange value
+     */
+    protected fun onChange(selfChange: Boolean) {
+        synchronized(selfObserverLock) {
+            contentObservable.dispatchChange(selfChange, null)
+            if (notifyUri != null && selfChange) {
+                contentResolver!!.notifyChange(notifyUri!!, selfObserver)
+            }
+        }
+    }
+
+    /**
+     * Specifies a content URI to watch for changes.
+     *
+     * @param cr The content resolver from the caller's context.
+     * @param notifyUri The URI to watch for changes. This can be a
+     * specific row URI, or a base URI for a whole class of content.
+     */
+    override fun setNotificationUri(cr: ContentResolver, notifyUri: Uri) = synchronized(selfObserverLock) {
+        this.contentResolver = cr
+        this.notifyUri = notifyUri
+
+        selfObserver?.let {
+            cr.unregisterContentObserver(it)
+        }
+        SelfContentObserver(this).let {
+            selfObserver = it
+            cr.registerContentObserver(notifyUri, true, it)
+        }
+        selfObserverRegistered = true
+    }
+
+    override fun getNotificationUri(): Uri = synchronized(selfObserverLock) { notifyUri!! }
+
+    override fun getWantsAllOnMoveCalls(): Boolean = false
+
+    override fun setExtras(extras: Bundle?) {
+        throw UnsupportedOperationException("Extras on cursor not supported")
+    }
+
+    override fun getExtras(): Bundle {
+        throw UnsupportedOperationException("Extras on cursor not supported")
+    }
+
+    override fun respond(extras: Bundle): Bundle {
+        throw UnsupportedOperationException("Extras on cursor not supported")
+    }
+
+    protected open fun finalize() {
+        if (selfObserver != null && selfObserverRegistered) {
+            contentResolver!!.unregisterContentObserver(selfObserver!!)
+        }
+        try {
+            if (!closed) {
+                close()
+            }
+        } catch (ignored: Exception) {
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun deactivate() {
+        onDeactivateOrClose()
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun requery(): Boolean = throw UnsupportedOperationException("Not supported")
+
+    override fun isClosed(): Boolean = closed
+
+    override fun close() {
+        closed = true
+        contentObservable.unregisterAll()
+        onDeactivateOrClose()
+    }
+
+    private fun onDeactivateOrClose() {
+        if (selfObserver != null) {
+            contentResolver!!.unregisterContentObserver(selfObserver!!)
+            selfObserverRegistered = false
+        }
+        dataSetObservable.notifyInvalidated()
         window = null
+    }
+
+    /**
+     * Cursors use this class to track changes others make to their URI.
+     */
+    private class SelfContentObserver(cursor: AbstractWindowedCursor) : ContentObserver(null) {
+        var cursor: WeakReference<AbstractWindowedCursor> = WeakReference(cursor)
+
+        override fun deliverSelfNotifications(): Boolean = false
+
+        override fun onChange(selfChange: Boolean) {
+            val cursor = cursor.get()
+            cursor?.onChange(false)
+        }
     }
 }
