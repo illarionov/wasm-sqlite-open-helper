d77089d616d5ab7e55de21cd2f1c749cd194dc65
Alexey Illarionov <alexey@0xdc.ru>
2024-03-23 01:45:29 +0300

    Add multithreading builds

--- sqlite-wasm/build.gradle.kts
+++ sqlite-wasm/build.gradle.kts
@@ -6,9 +6,12 @@
 
 @file:Suppress("GENERIC_VARIABLE_WRONG_DECLARATION")
 
+import ru.pixnews.wasm.sqlite.open.helper.builder.sqlite.SqliteCodeGenerationOptions
+import ru.pixnews.wasm.sqlite.open.helper.builder.sqlite.SqliteConfigurationOptions
+
+
 plugins {
     id("ru.pixnews.sqlite-wasm-builder")
-    id("ru.pixnews.wasm-sqlite-open-helper.gradle.lint.binary.compatibility.validator")
     id("ru.pixnews.wasm-sqlite-open-helper.gradle.multiplatform.kotlin")
     id("ru.pixnews.wasm-sqlite-open-helper.gradle.multiplatform.publish")
 }
@@ -21,10 +24,34 @@ version = wasmSqliteVersions.getSubmoduleVersionProvider(
     envVariableName = "WSOH_SQLITE_WASM_VERSION",
 ).get()
 
+val androidSaliteSpecifics = listOf(
+    "-DSQLITE_DEFAULT_AUTOVACUUM=1",
+    "-DSQLITE_DEFAULT_FILE_FORMAT=4",
+    "-DSQLITE_DEFAULT_FILE_PERMISSIONS=0600",
+    "-DSQLITE_DEFAULT_JOURNAL_SIZE_LIMIT=1048576",
+    "-DSQLITE_ENABLE_BATCH_ATOMIC_WRITE",
+    "-DSQLITE_ENABLE_MEMORY_MANAGEMENT=1",
+)
+
 sqlite3Build {
     builds {
         create("main") {
             sqliteVersion = defaultSqliteVersion
+        }
+        create("main-mt") {
+            sqliteVersion = defaultSqliteVersion
+            sqliteConfigOptions = SqliteConfigurationOptions.wasmConfig -
+                    listOf("-DSQLITE_THREADSAFE=0") +
+                    listOf("-DSQLITE_THREADSAFE=2")
+        }
+        create("main-mt-pthread") {
+            sqliteVersion = defaultSqliteVersion
+            codeGenerationOptions = SqliteCodeGenerationOptions.codeGenerationOptions + listOf(
+                "-pthread",
+            )
+            sqliteConfigOptions = SqliteConfigurationOptions.wasmConfig -
+                    listOf("-DSQLITE_THREADSAFE=0") +
+                    listOf("-DSQLITE_THREADSAFE=2")
         }
     }
 }
--- sqlite-wasm/src/jvmMain/kotlin/ru/pixnews/wasm/sqlite/open/helper/Sqlite3Wasm.kt
+++ sqlite-wasm/src/jvmMain/kotlin/ru/pixnews/wasm/sqlite/open/helper/Sqlite3Wasm.kt
@@ -12,6 +12,10 @@ public object Sqlite3Wasm {
     public object Emscripten {
         public val sqlite3_345: URL
             get() = getUrl("sqlite3-main-3450100.wasm")
+        public val sqlite3_345_mt: URL
+            get() = getUrl("sqlite3-main-mt-3450100.wasm")
+        public val sqlite3_345_mt_pthread: URL
+            get() = getUrl("sqlite3-main-mt-3450100.wasm")
     }
 
     private fun getUrl(fileName: String): URL = requireNotNull(Sqlite3Wasm::class.java.getResource(fileName))
