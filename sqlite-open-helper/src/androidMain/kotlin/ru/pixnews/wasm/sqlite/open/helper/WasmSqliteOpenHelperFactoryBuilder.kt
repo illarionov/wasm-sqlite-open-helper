23949ff754935e9060b343912f1682e31effadd5
Alexey Illarionov <alexey@0xdc.ru>
2024-04-14 07:47:05 +0300

    Add Android factory

--- sqlite-open-helper/src/androidMain/kotlin/ru/pixnews/wasm/sqlite/open/helper/WasmSqliteOpenHelperFactoryBuilder.kt
+++ sqlite-open-helper/src/androidMain/kotlin/ru/pixnews/wasm/sqlite/open/helper/WasmSqliteOpenHelperFactoryBuilder.kt
@@ -4,8 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+@file:Suppress("FunctionName")
+
 package ru.pixnews.wasm.sqlite.open.helper
 
+import android.content.Context
 import androidx.sqlite.db.SupportSQLiteOpenHelper
 import ru.pixnews.wasm.sqlite.open.helper.common.api.Logger
 import ru.pixnews.wasm.sqlite.open.helper.dsl.DebugConfigBlock
@@ -16,19 +19,49 @@ import ru.pixnews.wasm.sqlite.open.helper.embedder.SqliteEmbedderConfig
 import ru.pixnews.wasm.sqlite.open.helper.embedder.WasmSqliteCommonConfig
 import ru.pixnews.wasm.sqlite.open.helper.internal.CloseGuard
 import ru.pixnews.wasm.sqlite.open.helper.internal.CloseGuard.Reporter
+import ru.pixnews.wasm.sqlite.open.helper.path.AndroidDatabasePathResolver
+import ru.pixnews.wasm.sqlite.open.helper.path.DatabasePathResolver
 import ru.pixnews.wasm.sqlite.open.helper.path.JvmDatabasePathResolver
 
 /**
  * Creates a [SupportSQLiteOpenHelper.Factory] with the specified [block] configuration.
  *
- * @param sqliteCapi Implementation of the required functions from Sqlite C API. For example, GraalvmSqliteCapi
+ * @param context Application context
+ * @param embedder WebAssembly Runtime (embedder) for running SQLite compiled to Wasm.
+ * For example, GraalvmSqliteEmbedder
+ *
  */
-@Suppress("FunctionName")
+public fun <E : SqliteEmbedderConfig> WasmSqliteOpenHelperFactory(
+    embedder: SqliteEmbedder<E>,
+    context: Context,
+    block: WasmSqliteOpenHelperFactoryConfigBlock<E>.() -> Unit,
+): SupportSQLiteOpenHelper.Factory = WasmSqliteOpenHelperFactory(
+    embedder = embedder,
+    defaultPathResolver = AndroidDatabasePathResolver(context),
+    block = block,
+)
+
+/**
+ * Creates a [SupportSQLiteOpenHelper.Factory] with the specified [block] configuration.
+ *
+ * @param embedder WebAssembly Runtime (embedder) for running SQLite compiled to Wasm.
+ * For example, GraalvmSqliteEmbedder
+ */
 public fun <E : SqliteEmbedderConfig> WasmSqliteOpenHelperFactory(
     embedder: SqliteEmbedder<E>,
     block: WasmSqliteOpenHelperFactoryConfigBlock<E>.() -> Unit,
+): SupportSQLiteOpenHelper.Factory = WasmSqliteOpenHelperFactory(
+    embedder = embedder,
+    defaultPathResolver = JvmDatabasePathResolver(),
+    block = block,
+)
+
+internal fun <E : SqliteEmbedderConfig> WasmSqliteOpenHelperFactory(
+    embedder: SqliteEmbedder<E>,
+    defaultPathResolver: DatabasePathResolver,
+    block: WasmSqliteOpenHelperFactoryConfigBlock<E>.() -> Unit,
 ): SupportSQLiteOpenHelper.Factory {
-    val config = WasmSqliteOpenHelperFactoryConfigBlock<E>(JvmDatabasePathResolver()).apply(block)
+    val config = WasmSqliteOpenHelperFactoryConfigBlock<E>(defaultPathResolver).apply(block)
     val commonConfig = object : WasmSqliteCommonConfig {
         override val logger: Logger = config.logger
     }
