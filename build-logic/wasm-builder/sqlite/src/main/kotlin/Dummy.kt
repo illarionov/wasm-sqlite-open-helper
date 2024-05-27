5a5832a65825d4b4447921cef793ec81e7b3746c
Alexey Illarionov <alexey@0xdc.ru>
2024-05-27 18:36:51 +0300

    Remove common package from paths following Kotlin Code Conventions (#110)

---
+++ build-logic/wasm-builder/sqlite/src/main/kotlin/Dummy.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2024, the wasm-sqlite-open-helper project authors and contributors. Please see the AUTHORS file
+ * for details. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+@file:Suppress("FILE_CONTAINS_ONLY_COMMENTS", "FILE_NO_BLANK_LINE_BETWEEN_BLOCKS")
+
+// Workaround for https://youtrack.jetbrains.com/issue/KTIJ-15797
+package ru.pixnews.wasm.builder
