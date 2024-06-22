323cb253bac83b17cf1699b2c1d6d433f28714f8
Alexey Illarionov <alexey@0xdc.ru>
2024-06-22 13:54:45 +0300

    Bump dependency versions. (#142)
    * Gradle 8.9-rc1
    * Kotlin 2.0.20-Beta1
    * kotlinx0coroutines 1.9.0-RC
    * kermit 2.0.4
    * spotless 7.0.0.BETA1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,13 +13,13 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.room:androidx.room.gradle.plugin:2.7.0-alpha02")
+        classpath("androidx.room:androidx.room.gradle.plugin:2.7.0-alpha04")
         classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("com.diffplug.spotless:spotless-plugin-gradle:6.25.0")
-        classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.0.0-1.0.21")
+        classpath("com.diffplug.spotless:spotless-plugin-gradle:7.0.0.BETA1")
+        classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.0.20-Beta1-1.0.22")
         classpath("com.saveourtool.diktat:diktat-gradle-plugin:2.0.0")
         classpath("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20-Beta1")
         classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.24.0")
     }
 }
