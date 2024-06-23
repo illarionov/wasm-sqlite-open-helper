6dea3d63a110bddd2dbc1a762c9d1c47c9617e97
Alexey Illarionov <alexey@0xdc.ru>
2024-06-23 12:14:59 +0300

    Use builder in Graalvm and Chicory

--- sqlite-embedder-graalvm/src/jvmMain/kotlin/GraalvmSqliteEmbedderConfig.kt
+++ sqlite-embedder-graalvm/src/jvmMain/kotlin/GraalvmSqliteEmbedderConfig.kt
@@ -13,7 +13,6 @@ import ru.pixnews.wasm.sqlite.open.helper.WasmSqliteOpenHelperDsl
 import ru.pixnews.wasm.sqlite.open.helper.common.api.Logger
 import ru.pixnews.wasm.sqlite.open.helper.embedder.SqliteEmbedderConfig
 import ru.pixnews.wasm.sqlite.open.helper.host.EmbedderHost
-import ru.pixnews.wasm.sqlite.open.helper.host.jvm.JvmEmbedderHost
 
 /**
  * Configuration of the GraalVM engine
@@ -40,5 +39,5 @@ public class GraalvmSqliteEmbedderConfig internal constructor(
     /**
      * Implementation of a host object that provides access from the WebAssembly to external host resources.
      */
-    public var host: EmbedderHost = JvmEmbedderHost(rootLogger = embedderRootLogger)
+    public var host: EmbedderHost = EmbedderHost.Builder().apply { this.rootLogger = embedderRootLogger }.build()
 }
