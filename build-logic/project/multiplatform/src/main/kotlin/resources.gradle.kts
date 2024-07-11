8ade925f034af4a4ba35acfaf0b5756b809290de
Alexey Illarionov <alexey@0xdc.ru>
2024-07-11 09:20:19 +0300

    Remove compose-compiler

--- build-logic/project/multiplatform/src/main/kotlin/resources.gradle.kts
+++ build-logic/project/multiplatform/src/main/kotlin/resources.gradle.kts
@@ -6,30 +6,52 @@
 
 package ru.pixnews.wasm.sqlite.open.helper.gradle.multiplatform
 
-import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.ComposeKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-
-plugins {
-    id("org.jetbrains.compose")
-    id("org.jetbrains.kotlin.plugin.compose")
-}
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
+import org.jetbrains.kotlin.gradle.plugin.extraProperties
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJsCompilation
+import org.jetbrains.kotlin.gradle.plugin.mpp.resources.KotlinTargetResourcesPublication
 
 /*
- * Convention plugin that acivates kotlin multiplatform resources
+ * Convention plugin that activates kotlin multiplatform resources
+ *
+ * Source: https://github.com/JetBrains/compose-multiplatform/blob/5da48904e5c24aa567da17fb780707073f920af3/gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/resources/KmpResources.kt
  */
+@OptIn(ComposeKotlinGradlePluginApi::class)
 plugins.withId("org.jetbrains.kotlin.multiplatform") {
-    extensions.configure<KotlinMultiplatformExtension> {
-        sourceSets {
-            commonMain.dependencies {
-                compileOnly(compose.dependencies.runtime)
-            }
-            commonTest.dependencies {
-                compileOnly(compose.dependencies.runtime)
+    val platformsForSetupCompilation = listOf(KotlinPlatformType.native, KotlinPlatformType.js, KotlinPlatformType.wasm)
+    val kotlinExtension: KotlinMultiplatformExtension = extensions.getByType()
+    val kmpResources = extraProperties.get("multiplatformResourcesPublication") as KotlinTargetResourcesPublication
+
+    kotlinExtension.targets
+        .matching { target -> target.platformType in platformsForSetupCompilation }
+        .configureEach {
+            val allResources = kmpResources.resolveResources(this)
+            compilations.all {
+                if (this.name == "main") {
+                    configureResourcesForCompilation(this, allResources)
+                }
             }
         }
-    }
 }
 
-composeCompiler {
-    targetKotlinPlatforms.set(emptySet())
+/**
+ * Add resolved resources to a kotlin compilation to include it into a resulting platform artefact
+ * It is required for JS and Native targets.
+ * For JVM and Android it works automatically via jar files
+ */
+private fun Project.configureResourcesForCompilation(
+    compilation: KotlinCompilation<*>,
+    directoryWithAllResourcesForCompilation: Provider<File>,
+) {
+    compilation.defaultSourceSet.resources.srcDir(directoryWithAllResourcesForCompilation)
+
+    // JS packaging requires explicit dependency
+    if (compilation is KotlinJsCompilation) {
+        tasks.named(compilation.processResourcesTaskName).configure {
+            dependsOn(directoryWithAllResourcesForCompilation)
+        }
+    }
 }
