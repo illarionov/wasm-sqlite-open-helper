55cc4b9dfb311ec2ce61c664e2f3d12a51f18dc8
Alexey Illarionov <alexey@0xdc.ru>
2024-08-25 16:07:40 +0300

    Add more tests to linux target

---
+++ sqlite-driver/src/linuxTest/kotlin/chasm/ChasmIcuCollationTest.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2024, the wasm-sqlite-open-helper project authors and contributors. Please see the AUTHORS file
+ * for details. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package ru.pixnews.wasm.sqlite.driver.chasm
+
+import ru.pixnews.wasm.sqlite.driver.WasmSQLiteDriver
+import ru.pixnews.wasm.sqlite.driver.test.base.tests.AbstractIcuCollationTest
+import ru.pixnews.wasm.sqlite.test.utils.TempFolder
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+
+class ChasmIcuCollationTest : AbstractIcuCollationTest<WasmSQLiteDriver<*>>(
+    driverCreator = ChasmSqliteDriverFactory,
+) {
+    private lateinit var tempDir: TempFolder
+
+    @BeforeTest
+    fun setupTempDir() {
+        tempDir = TempFolder.create()
+    }
+
+    @AfterTest
+    fun cleanupTempDir() {
+        tempDir.delete()
+    }
+
+    override fun fileInTempDir(databaseName: String): String = tempDir.resolve(databaseName)
+}
