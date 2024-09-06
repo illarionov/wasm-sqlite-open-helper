60ed35884cdd178cf7348a011fc3c35a1f06b9c1
Alexey Illarionov <alexey@0xdc.ru>
2024-09-06 19:10:47 +0300

    Bump dependency versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     }
     dependencies {
         classpath("androidx.room:androidx.room.gradle.plugin:2.7.0-alpha07")
-        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("com.android.tools.build:gradle:8.6.0")
         classpath("com.diffplug.spotless:spotless-plugin-gradle:7.0.0.BETA2")
         classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.0.20-1.0.24")
         classpath("com.saveourtool.diktat:diktat-gradle-plugin:2.0.0")
