1294d81c201793405fb445f095178cdb0ca51808
Alexey Illarionov <alexey@0xdc.ru>
2025-03-04 08:59:13 +0300

    Update Gradle, tune Github Actions

--- build-logic/project/lint/src/main/kotlin/LintConfig.kt
+++ build-logic/project/lint/src/main/kotlin/LintConfig.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024, the wasm-sqlite-open-helper project authors and contributors. Please see the AUTHORS file
+ * Copyright 2024-2025, the wasm-sqlite-open-helper project authors and contributors. Please see the AUTHORS file
  * for details. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -11,13 +11,13 @@ import org.gradle.api.file.Directory
 import org.gradle.api.file.FileTree
 import org.gradle.api.tasks.util.PatternFilterable
 
+@Suppress("UnstableApiUsage")
 internal val Project.configRootDir: Directory
-    get() {
-        return rootProject.layout.projectDirectory.dir("config")
-    }
+    get() = layout.settingsDirectory.dir("config")
 
+@Suppress("UnstableApiUsage")
 internal val Project.lintedFileTree: FileTree
-    get() = rootProject.layout.projectDirectory.asFileTree.matching {
+    get() = layout.settingsDirectory.asFileTree.matching {
         excludeNonLintedDirectories()
     }
 
