9672b8a17bbdf3626775d52bb4df48853a6cfe73
Alexey Illarionov <alexey@0xdc.ru>
2025-04-11 05:28:31 +0300

    Bump dependency versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
     id("at.released.wasm.sqlite.open.helper.gradle.settings.root")
 }
 
@@ -14,13 +14,13 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.room:androidx.room.gradle.plugin:2.7.0-rc02")
+        classpath("androidx.room:androidx.room.gradle.plugin:2.7.0")
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.diffplug.spotless:spotless-plugin-gradle:7.0.2")
-        classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.1.10-1.0.31")
+        classpath("com.diffplug.spotless:spotless-plugin-gradle:7.0.3")
+        classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.1.20-2.0.0")
         classpath("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.8")
         classpath("at.released.cassettes:cassettes-plugin:0.1-alpha01")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
         classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.27.0")
     }
 }
