ade77e60c8f7074c92c9375eb05f63ab75b51890
Alexey Illarionov <alexey@0xdc.ru>
2025-04-12 18:57:53 +0300

    Add gtag

--- doc/website/docusaurus.config.ts
+++ doc/website/docusaurus.config.ts
@@ -51,6 +51,10 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-178N793F0M',
+          anonymizeIP: true,
+        },
       } satisfies Preset.Options,
     ],
   ],
